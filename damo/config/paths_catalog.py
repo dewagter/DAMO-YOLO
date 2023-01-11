6b3f110b2c55b65d6ea80a4d886daf97375990eb
Christophe De Wagter <dewagter@gmail.com>
2023-01-11 20:58:40 +0100

    2) configure data path

--- damo/config/paths_catalog.py
+++ damo/config/paths_catalog.py
@@ -7,17 +7,13 @@ import os
 class DatasetCatalog(object):
     DATA_DIR = 'datasets'
     DATASETS = {
-        'coco_2017_train': {
-            'img_dir': 'coco/train2017',
-            'ann_file': 'coco/annotations/instances_train2017.json'
+        'coco_train': {
+            'img_dir': 'coco/images',
+            'ann_file': 'coco/annotations/train.json'
         },
-        'coco_2017_val': {
-            'img_dir': 'coco/val2017',
-            'ann_file': 'coco/annotations/instances_val2017.json'
-        },
-        'coco_2017_test_dev': {
-            'img_dir': 'coco/test2017',
-            'ann_file': 'coco/annotations/image_info_test-dev2017.json'
+        'coco_val': {
+            'img_dir': 'coco/images',
+            'ann_file': 'coco/annotations/val.json'
         },
         }
 
